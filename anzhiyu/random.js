2f3b109232d74755a66f9fd2f42879fe5380dbe2
fishing886 <145727116+fishlanding@users.noreply.github.com>
2024-09-03 13:57:11 +0800

    Site updated: 2024-09-03 13:57:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/01/Bulk_Crap_Uninstaller/","2024/08/14/hello-world/"];function toRandomPost(){
+var posts=["2024/09/03/LeagueCraft/","2024/09/01/Bulk_Crap_Uninstaller/","2024/08/14/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

02711047e0248855823ecb66ca448152797da792
fishing886 <145727116+fishlanding@users.noreply.github.com>
2024-09-07 22:27:14 +0800

    Site updated: 2024-09-07 22:27:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/01/Bulk_Crap_Uninstaller/","2024/09/03/LeagueCraft/","2024/09/07/how_to_use_createchat/","2024/08/14/hello-world/"];function toRandomPost(){
+var posts=["2024/09/01/Bulk_Crap_Uninstaller/","2024/09/03/LeagueCraft/","2024/08/14/hello-world/","2024/09/07/how_to_use_createchat/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

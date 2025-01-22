dcfe6d11a299a62f16d84f19d8f155262bf0f42b
fishing886 <145727116+fishlanding@users.noreply.github.com>
2025-01-22 18:31:18 +0800

    Site updated: 2025-01-22 18:31:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/01/Bulk_Crap_Uninstaller/","2024/08/14/hello-world/","2024/09/03/LeagueCraft/","2024/09/07/how_to_use_createchat/"];function toRandomPost(){
+var posts=["2024/09/03/LeagueCraft/","2024/09/01/Bulk_Crap_Uninstaller/","2024/08/14/hello-world/","2024/09/07/how_to_use_createchat/","2025/01/22/snowy-skirmish/","2025/01/22/ttr/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
